d85ef6ad8edee8e585f790d951740911578819fb
= <help.studentgrade@gmail.com>
2022-06-27 23:03:46 +0600

    added how to install bootstrap in react

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>Hello World React !</h2>
+      <h2 className='bg-info'>Hello World React !</h2>
     </div>
   );
 }
