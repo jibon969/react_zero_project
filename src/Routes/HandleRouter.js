e38a7af85439e2833cce537e0d09d44c07d44abc
= <help.studentgrade@gmail.com>
2022-07-16 15:20:37 +0600

    added layout

--- src/Routes/HandleRouter.js
+++ src/Routes/HandleRouter.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "../pages/Home/Home";
-import About from "../pages/About/About";
+import Home from "../views/Home/Home";
+import About from "../views/About/About";
 
 const HandleRouter = () => {
   return (
