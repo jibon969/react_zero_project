94169242bd311272ebbc8cf4c82ca9a78cb3b9ac
= <help.studentgrade@gmail.com>
2022-07-17 23:37:30 +0600

    added cdn link and others css

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import "./About.style.css";
 
-function About(props) {
-  return (
-    <div>
-      <h2>About Page</h2>
-      <Link to="/">About</Link>
-    </div>
-  );
-}
+const About = (props) => {
+    return (
+        <>
+            <section id="aboutSection">
+                <div className="container">
+                    <h3 className="aboutTitle">About Us</h3>
+                    <div className="row">
+                        <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                            <div className="aboutContent">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci assumenda beatae commodi deserunt libero nam numquam obcaecati! Ad adipisci deserunt dignissimos distinctio eaque in iste iusto labore laboriosam minus, molestiae mollitia nulla provident quasi repellat velit veritatis. Ab aperiam asperiores fuga harum illo incidunt natus porro praesentium quae quis, soluta, totam vel velit, veritatis vitae. Consequuntur eveniet iste nemo, repudiandae tempora tenetur. Accusantium alias aspernatur, autem commodi culpa cupiditate debitis dolorem ducimus eaque eligendi enim error eveniet id ipsam minima minus, nostrum numquam officiis pariatur placeat porro provident quaerat quia reprehenderit sed, vel veritatis voluptatem. Cupiditate distinctio doloremque repudiandae vel.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+};
 
 export default About;
